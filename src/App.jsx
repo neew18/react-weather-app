9e6fa127eabab0ebca40cd6c7baca4c6fa9bfda6
Nann Ei Ei Win <nanneieiwinn37@gmail.com>
2023-03-09 00:47:55 +0630

    Add Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Footer from "./Footer";
 import Weather from "./Weather";
 import "./Weather.css";
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="container App">
       <Weather />
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="position-fixed fixed-bottom text-center Footer">
+      <p>
+        Coded by{" "}
+        <a href="https://neew18.github.io/neew-links-tree/">Nann Ei Ei Win</a>{" "}
+        and is{" "}
+        <a href="https://github.com/neew18/react-weather-app">open-source</a>
+      </p>
+    </div>
+  );
+}
+
+export default Footer;
